41a27280c9ff25073869deaf0e40c986a84196e1
Requinard <d.diks@mail.blastedsolutions.com>
2015-03-13 13:05:08 +0100

    Added base for DBManager so it'll connect

--- ICT4EVENT/DBManager.cs
+++ ICT4EVENT/DBManager.cs
@@ -4,16 +4,24 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ICT4EVENT
 {
     class DBManager
     {
-        private SqlConnection sqlConnection;
+        private MySqlConnection dbConnection;
 
         public DBManager()
         {
-            Console.WriteLine("i");
+            dbConnection = new MySqlConnection();
+            dbConnection.ConnectionString = "SERVER=terarion.com;DATABASE=proftaak2;UID=proftaak2;PASSWORD=FCUGBmvCFJfyGNv6;";
+            dbConnection.Open();
+        }
+
+        private void QueryDB(string query)
+        {
+            throw new NotImplementedException();
         }
     }
 }
