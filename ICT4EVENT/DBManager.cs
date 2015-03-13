06b40ad7483931d57d1d717a166dc25d699d0b7b
Requinard <d.diks@mail.blastedsolutions.com>
2015-03-13 10:15:24 +0100

    Added DBManager

---
+++ ICT4EVENT/DBManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4EVENT
+{
+    class DBManager
+    {
+        private SqlConnection sqlConnection;
+
+        public DBManager()
+        {
+            Console.WriteLine("i");
+        }
+    }
+}
