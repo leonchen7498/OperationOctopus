9a1a0dfe23ff3909b6089b6abedaa4cd97cdcb49
Requinard <d.diks@mail.blastedsolutions.com>
2015-03-13 10:14:12 +0100

    Added event class to demonstrate inheritance and interfaces

--- ICT4EVENT/DBModel.cs
+++ ICT4EVENT/DBModel.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace ICT4EVENT
 {
-    class DBModel
+    abstract class DBModel
     {
+        private DBManager dbManager;
+
+        public DBModel(DBManager dbManager)
+        {
+            this.dbManager = dbManager;
+        }
+    }
+
+    class EventModel : DBModel, IDataModelUpdate
+    {
+        public EventModel(DBManager dbManager) :base (dbManager)
+        {
+            
+        }
+
+        public bool Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Destroy()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
